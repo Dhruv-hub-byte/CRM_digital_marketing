693f49a6aa3fc990f649737af14e73ffbbf6740c
Dhruv bansal <cybosecdhruv@gmail.com>
2026-06-17 13:16:37 +0530

    add email notification sedgridsss

--- frontend/src/hooks/useToastContext.js
+++ frontend/src/hooks/useToastContext.js
@@ -2,5 +2,8 @@ import { useContext } from 'react';
 import { ToastContext } from '../components/Layout';
 
 export default function useToastContext() {
-  return useContext(ToastContext);
+  const context = useContext(ToastContext);
+  // Return a no-op function if context not available
+  // prevents crash if called outside Layout
+  return context || (() => {});
 }
